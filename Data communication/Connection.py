ba4e4cd8807603e99163bb06e190d752cdcf84d6
VictorAldica <victor9753110@gmail.com>
2023-05-23 10:12:49 +0300

    Implemented timeout checker

--- Data communication/Connection.py
+++ Data communication/Connection.py
@@ -1,6 +1,43 @@
 import serial
+import time
+from serial.serialutil import SerialException
+
+class SerialCommunication:
+    def __init__(self, port, baud_rate=9600, timeout=1):
+        self.port = port
+        self.baud_rate = baud_rate
+        self.timeout = timeout
+        self.serial_port = None
+        self.connect()
+
+    def connect(self):
+        try:
+            self.serial_port = serial.Serial(self.port, self.baud_rate, timeout=self.timeout)
+            print(f"Connected to Arduino on port {self.port}")
+        except SerialException:
+            print(f"Arduino not found on port {self.port}")
+            self.serial_port = None
+
+    def send(self, message):
+        if self.serial_port is None:
+            print(f"No connection to Arduino on port {self.port}. Attempting to reconnect...")
+            self.connect()
+            if self.serial_port is None:
+                print(f"Failed to reconnect to Arduino on port {self.port}")
+                return False
+
+        try:
+            self.serial_port.write(str(message).encode())
+            time.sleep(0.05)  
+            return True
+        except SerialException:
+            print(f"Lost connection to Arduino on port {self.port}")
+            self.serial_port = None
+            return False
+
+arduino_communicator = SerialCommunication('/dev/ttyACM0')  
 
 def send_to_arduino(distance):
-    ser = serial.Serial('/dev/ttyACM0', 9600) 
-    ser.write(str(distance).encode())
-    ser.close()
+    success = arduino_communicator.send(distance)
+    if not success:
+        print("Failed to send data to Arduino")
