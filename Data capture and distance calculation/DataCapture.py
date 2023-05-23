bd925ffbf0079a6a9e59571b9e8f5cb6577cdd62
VictorAldica <victor9753110@gmail.com>
2023-05-23 10:19:29 +0300

    More complex calculations, added minimalist GUI for debugging outside SDK

--- Data capture and distance calculation/DataCapture.py
+++ Data capture and distance calculation/DataCapture.py
@@ -1,24 +1,54 @@
 import pyrealsense2 as rs
 import numpy as np
+import cv2
 
 pipeline = rs.pipeline()
 config = rs.config()
 
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+
 profile = pipeline.start(config)
+
+
 depth_sensor = profile.get_device().first_depth_sensor()
 depth_scale = depth_sensor.get_depth_scale()
 
-def calculate_distance(depth_image, x, y):
-    return depth_image[y, x] * depth_scale
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+def calculate_distance(depth_image, depth_scale):
+    depth_in_meters = depth_image * depth_scale
+
+    min_distance = np.min(depth_in_meters)
+    min_distance_coordinates = np.unravel_index(np.argmin(depth_in_meters), depth_in_meters.shape)
+
+    return min_distance, min_distance_coordinates
 
 try:
     while True:
         frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
+
+        aligned_frames = align.process(frames)
+
+        depth_frame = aligned_frames.get_depth_frame()
+
+        if not depth_frame:
+            continue
+
         depth_image = np.asanyarray(depth_frame.get_data())
-        distance = calculate_distance(depth_image, depth_image.shape[1]//2, depth_image.shape[0]//2)
-        
-        send_to_arduino(distance)
+
+        min_distance, min_distance_coordinates = calculate_distance(depth_image, depth_scale)
+
+        send_to_arduino(min_distance)
+
+        cv2.imshow('Depth Image', depth_image)
+
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            break
 
 finally:
     pipeline.stop()
