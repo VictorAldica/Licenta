cb3b3a390af6487fc59d1fb68e687b7331e0ecf1
VictorAldica <victor9753110@gmail.com>
2023-05-23 10:04:57 +0300

    Refactor for multiple buzzers and motors

--- Signal conversion/Signal.cpp
+++ Signal conversion/Signal.cpp
@@ -1,18 +1,37 @@
-// code for arduino
-int buzzer = 9; // buzzer
-int motorPin = 10; // motor
+// Arduino code
+#define BUZZER_COUNT 2 
+#define MOTOR_COUNT 4 
+
+int buzzers[BUZZER_COUNT] = {9, 10}; 
+int motors[MOTOR_COUNT] = {5, 6, 7, 8};
 
 void setup() {
   Serial.begin(9600);
-  pinMode(buzzer, OUTPUT);
-  pinMode(motorPin, OUTPUT);
+  for(int i = 0; i < BUZZER_COUNT; i++) {
+    pinMode(buzzers[i], OUTPUT);
+  }
+  for(int i = 0; i < MOTOR_COUNT; i++) {
+    pinMode(motors[i], OUTPUT);
+  }
 }
+
+
+float filter_factor = 0.1; 
+float smoothed_intensity = 0.0;
 
 void loop() {
   if(Serial.available() > 0) {
     int distance = Serial.parseInt();
-    int intensity = map(distance, 0, 2000, 0, 255); 
-    analogWrite(motorPin, intensity);
-    tone(buzzer, intensity);
+    int intensity = map(distance, 0, 2000, 0, 255);
+
+    
+    smoothed_intensity = (1.0 - filter_factor) * smoothed_intensity + filter_factor * intensity;
+
+    for(int i = 0; i < BUZZER_COUNT; i++) {
+      tone(buzzers[i], smoothed_intensity);
+    }
+    for(int i = 0; i < MOTOR_COUNT; i++) {
+      analogWrite(motors[i], smoothed_intensity);
+    }
   }
 }
