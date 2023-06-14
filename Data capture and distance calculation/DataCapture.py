4574db040425e7b26c2ec4b7f59538d4bf67a7d9
VictorAldica <victor9753110@gmail.com>
2023-06-14 23:05:17 +0300

    Code cleanup and improvement

--- Data capture and distance calculation/DataCapture.py
+++ Data capture and distance calculation/DataCapture.py
@@ -3,9 +3,10 @@ import numpy as np
 import cv2
 import threading
 import time
+from collections import deque
 
 class FrameProcessor(threading.Thread):
-    def __init__(self):
+    def __init__(self, buffer_length=10, pit_threshold=1.5):
         super(FrameProcessor, self).__init__()
         self.pipeline = rs.pipeline()
         self.config = rs.config()
@@ -16,6 +17,9 @@ class FrameProcessor(threading.Thread):
         self._stop_event = threading.Event()
 
         self.width = 640
+        self.buffer_length = buffer_length
+        self.pit_threshold = pit_threshold
+        self.max_distance_history = deque(maxlen=buffer_length)
 
         self.config.enable_stream(rs.stream.depth, self.width, 480, rs.format.z16, 30)
         self.config.enable_stream(rs.stream.color, self.width, 480, rs.format.bgr8, 30)
@@ -46,10 +50,7 @@ class FrameProcessor(threading.Thread):
 
     def get_frames(self):
         with self.lock:
-            if self.color_image is not None and self.depth_image is not None:
-                return self.color_image.copy(), self.depth_image.copy()
-            else:
-                return None, None
+            return self.color_image.copy() if self.color_image is not None else None, self.depth_image.copy() if self.depth_image is not None else None
 
     def calculate_distance(self, depth_image):
         depth_in_meters = depth_image * self.depth_scale
@@ -64,6 +65,8 @@ class FrameProcessor(threading.Thread):
 
         section = self.get_section(min_distance_coordinates)
 
+        self.max_distance_history.append(max_distance)
+
         return min_distance, min_distance_coordinates, max_distance, max_distance_coordinates, section
 
     def get_section(self, coordinates):
@@ -77,37 +80,48 @@ class FrameProcessor(threading.Thread):
         else:
             return 'Right'
 
-frame_processor = FrameProcessor()
-frame_processor.start()
+    def check_for_pit(self, max_distance):
+        if len(self.max_distance_history) == self.buffer_length:
+            avg_distance = np.mean(self.max_distance_history)
+            if max_distance > self.pit_threshold * avg_distance:
+                return True
+        return False
 
-try:
-    while True:
-        color_image, depth_image = frame_processor.get_frames()
+if __name__ == "__main__":
+    frame_processor = FrameProcessor()
+    frame_processor.start()
 
-        if color_image is None or depth_image is None:
-            continue
+    try:
+        while True:
+            color_image, depth_image = frame_processor.get_frames()
 
-        min_distance, min_distance_coordinates, max_distance, max_distance_coordinates, section = frame_processor.calculate_distance(depth_image)
+            if color_image is None or depth_image is None:
+                continue
 
-        print(f"Min distance: {min_distance} m in section {section}")
-        print(f"Max distance: {max_distance} m")
+            min_distance, min_distance_coordinates, max_distance, max_distance_coordinates, section = frame_processor.calculate_distance(depth_image)
 
-        cv2.circle(color_image, (min_distance_coordinates[1], min_distance_coordinates[0]), 5, (0, 255, 0), -1)
-        cv2.putText(color_image, f"{min_distance} m", (min_distance_coordinates[1], min_distance_coordinates[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            print(f"Min distance: {min_distance} m in section {section}")
+            print(f"Max distance: {max_distance} m")
 
-        cv2.circle(color_image, (max_distance_coordinates[1], max_distance_coordinates[0]), 5, (0, 0, 255), -1)
-        cv2.putText(color_image, f"{max_distance} m", (max_distance_coordinates[1], max_distance_coordinates[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            if frame_processor.check_for_pit(max_distance):
+                print("Pit detected")
 
-        cv2.imshow('Color Image', color_image)
+            cv2.circle(color_image, (min_distance_coordinates[1], min_distance_coordinates[0]), 5, (0, 255, 0), -1)
+            cv2.putText(color_image, f"{min_distance} m", (min_distance_coordinates[1], min_distance_coordinates[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-        key = cv2.waitKey(1)
-        if key & 0xFF == ord('q') or key == 27:
-            cv2.destroyAllWindows()
-            break
-        elif key == ord('s'):
-            timestamp = time.strftime("%Y%m%d-%H%M%S")
-            cv2.imwrite(f"color_image_{timestamp}.png", color_image)
-            cv2.imwrite(f"depth_image_{timestamp}.png", depth_image)
+            cv2.circle(color_image, (max_distance_coordinates[1], max_distance_coordinates[0]), 5, (0, 0, 255), -1)
+            cv2.putText(color_image, f"{max_distance} m", (max_distance_coordinates[1], max_distance_coordinates[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-finally:
-    frame_processor.stop()
+            cv2.imshow('Color Image', color_image)
+
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q') or key == 27:
+                cv2.destroyAllWindows()
+                break
+            elif key == ord('s'):
+                timestamp = time.strftime("%Y%m%d-%H%M%S")
+                cv2.imwrite(f"color_image_{timestamp}.png", color_image)
+                cv2.imwrite(f"depth_image_{timestamp}.png", depth_image)
+
+    finally:
+        frame_processor.stop()
